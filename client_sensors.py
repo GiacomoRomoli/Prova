4f72c67d7925fe344949416b94114b378e410368
Giacomo <giacomoromoli.gr@gmail.com>
2024-04-05 17:28:23 +0200

    Initial Commit

---
+++ client_sensors.py
@@ -0,0 +1,16 @@
+from requests import get, post
+import time
+
+base_url = 'http://localhost:80'
+sensor = 's1'
+with open('CleanData_PM10.csv') as f: #vado a leggere il file csv, simulando l'arrivo di dati da parte di un sensore, per poi prendere questi dati e caricarli sul server
+    for l in f.readlines()[1:]: #salto la prima riga perché è quella di intestazione
+        data,val = l.strip().split(',')
+        print(data,val)
+        r = post(f'{base_url}/sensors/{sensor}',
+                 data={'data':data,'val':val}) #mando il valore 'val', alla data 'data', al sensore 'sensor'
+        time.sleep(5)
+
+
+
+print('done')
---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask,request,redirect,url_for #flask è una libreria che permette di creare applicazioni web
+import json
+
+app = Flask(__name__) #creo un'applicazione
+
+db = {}
+
+#devo andare a creare una cartella 'static' all'interno della quale vado ad inserire, oltre agli asset e le immagini che mi servono per la pagina web, il file html (che è un file statico, non è interattivo come un javascript o un python). Questo perché se lo tenessi all'interno della cartella dove stanno anche tutti gli altri file di python mi darebbe errore. Questo succede perché il server che mi ha fornito questa pagina web e il server che mi fornisce i dati sono su due server diversi, dunque il browser decide di bloccare la richiesta per problemi di privacy.
+@app.route('/graph')
+def graph():
+    return redirect(url_for('static', filename='graph.html')) #quando andrò all'url /graph, allora verrò ricondotto alla cartella 'static', al file 'graph.html'
+
+
+
+@app.route('/sensors',methods=['GET'])
+def main():
+    return json.dumps(list(db.keys())), 200
+
+
+@app.route('/sensors/<s>',methods=['POST'])
+def add_data(s):
+    data = request.values['data']
+    val = float(request.values['val'])
+    if s in db:
+        db[s].append((data,val))
+    else:
+        db[s] = [(data,val)]
+    return 'ok',200
+
+@app.route('/sensors/<s>',methods=['GET'])
+def get_data(s):
+    if s in db:
+        #return json.dumps(db[s])
+        r = []
+        for i in range(len(db[s])):
+            r.append([i,db[s][i][1]]) #vado a prendere il valore da un dizionario che è una lista di liste
+        return json.dumps(r),200
+    else:
+        return 'sensor not found',404
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True) #faccio partire un'applicazione web
+
