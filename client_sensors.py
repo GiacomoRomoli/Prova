4ef11a52de9a822ff6ce861e4da710b31d75d2ad
Giacomo <giacomoromoli.gr@gmail.com>
2024-04-06 20:23:58 +0200

    Initial Commit

--- client_sensors.py
+++ client_sensors.py
@@ -1,8 +1,8 @@
 from requests import get, post
 import time
 
-#base_url = 'http://34.154.157.58:80'
-base_url = 'http://localhost'
+base_url = 'http://34.154.157.58:80'
+#base_url = 'http://localhost'
 sensor = 's1'
 with open('CleanData_PM10.csv') as f: #vado a leggere il file csv, simulando l'arrivo di dati da parte di un sensore, per poi prendere questi dati e caricarli sul server
     for l in f.readlines()[1:]: #salto la prima riga perché è quella di intestazione
